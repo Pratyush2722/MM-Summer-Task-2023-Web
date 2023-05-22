c9fa17b561168d969b9b2c506c489b1bef79d62d
Pratyush2722 <129552249+Pratyush2722@users.noreply.github.com>
2023-05-22 19:57:56 +0530

    Started recently ,i will update it with due time .

---
+++ pratyush/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Navbar from './Home/Navbar'
+import { Box } from "@mui/material";
+function App() {
+  return (
+   <Box>
+    <Navbar/>
+   </Box>
+  )
+}
+
+export default App;
