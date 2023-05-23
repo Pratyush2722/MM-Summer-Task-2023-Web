ba623217b64304e4eacc711e4c657bb6dde5ee5f
Pratyush2722 <129552249+Pratyush2722@users.noreply.github.com>
2023-05-23 21:19:47 +0530

    updated

--- pratyush/src/App.js
+++ pratyush/src/App.js
@@ -1,13 +1,16 @@
 
 import React from "react";
 import Navbar from './Home/Navbar'
-import { Box } from "@mui/material";
+import { Box } from "@mui/material" 
+import Welcome from './components/Welcome'
 function App() {
   return (
    <Box>
     <Navbar/>
+    <Welcome/>
    </Box>
   )
 }
+
 
 export default App;
